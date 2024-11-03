a7a922c8a6f25dde3a0f8b6247dadf33a6dc6f65
Taiwo <olajolotaiwo2@gmail.com>
2024-11-03 02:50:04 +0100

    Add files via upload

---
+++ 04lesson/exercise.js
@@ -0,0 +1,20 @@
+//exercise a and b
+const name ='Timilehin';
+
+console.log(`My name is: ${name}`)
+
+// exercise c  
+const coffee = 5
+const bagels = 3
+const soup = 9
+const tax = 0.1
+
+const cost = coffee + (2 * bagels) + soup
+
+console.log(`cost: $${cost}`)
+
+console.log(`tax(10%): $${cost * tax}`)
+
+
+
+let cartQuantity = 0;
---
+++ 04lesson/index.js
@@ -0,0 +1,43 @@
+let cartQuantity = 0;
+
+//Naming Convention
+// camelCase ; every word can be capitalize expect the first word.
+// PascalCase: the first word can be capitalized
+//kebab-case : lowercase combined with a dash, it does words in js, used in HTML and CSS
+//snake_case: lowercase combined with underscore. It is used in other languages 
+
+//shortcut for other operators
+
+// variable = variable + 2
+//variable +=2
+
+// variable = variable - 2
+//variable -=2
+
+// variable = variable * 2
+//variable *=2
+
+// variable = variable / 2
+//variable /=2
+
+// variable = variable - 1
+//variable-- 
+//variable-= 1
+
+// variable = variable + 1
+//variable++
+//variable+= 2
+
+// let const and var
+// const variable can't be changed later, it is very easy to understand
+// let variable are used went variable are need to be changed later
+// var is similar to let. it is the original way to create variable in js. it is not use in new js.
+
+// typeof can also be used
+// console.log(typeof variable2);
+// console.log(typeof message);
+
+//example;
+//const variable2 = 3;
+// variable2 = 3;
+// it doesn't work that way
